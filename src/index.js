ff8871e30642aad85c0776e3e96435185f279d75
ZuhaibHunzai <zuhaibalamhunzai188@gmail.com>
2024-03-28 01:20:23 +0500

    more chains added

--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ import reportWebVitals from "./reportWebVitals";
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
-import { mainnet, polygon, avalanche } from "wagmi/chains";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  zora,
+  avalanche,
+} from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 const config = getDefaultConfig({
-  appName: "Token Migrator",
-  projectId: "7e778a0cc9adc4e4434bf73bff51f07c", //update project id
-  chains: [mainnet, polygon, avalanche],
+  appName: "Avax Staking",
+  projectId: "7e778a0cc9adc4e4434bf73bff51f07c",
+  chains: [mainnet, polygon, optimism, arbitrum, base, zora, avalanche],
   ssr: true,
 });
 const queryClient = new QueryClient();
